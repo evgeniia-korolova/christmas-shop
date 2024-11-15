0ccf13cee63f52ac801cb0f5fa9a63e90a68f5fe
evgeniia-korolova <jkod@ukr.net>
2024-11-15 03:49:52 +0200

    add close burger on kinks click

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,14 +1,20 @@
 export default function toggleBurger() {
 
-    const burgerBtn = document.querySelector('.hamburger-open');
-    
-    const menu = document.querySelector('.navigation');
-    const burgerSpans = document.querySelectorAll('.humburger__line');
+    const burgerBtn = document.querySelector('.hamburger-open');    
+    const menu = document.querySelector('.navigation');    
+    const navLinks = document.querySelectorAll('.nav__link');
     
     burgerBtn.addEventListener('click', () => {
         menu.classList.toggle('is-open');
         document.body.classList.toggle('no-scroll');
     })
+
+    navLinks.forEach((element) => {
+			element.addEventListener('click', function () {
+				menu.classList.remove('is-open');
+				document.body.classList.toggle('no-scroll');
+			});
+		});
 }
 
 
